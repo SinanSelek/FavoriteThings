ab36192d0d5f1e9db467844b0ee381732b6b9a61
Sinan Selek <sselek62@gmail.com>
2023-02-28 20:06:56 +0300

    Initial Commit

---
+++ FavoriteThings/FavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsApp.swift
+//  FavoriteThings
+//
+//  Created by Sinan Selek on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
